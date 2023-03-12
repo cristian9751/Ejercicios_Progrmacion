37694fd513f10a58b1d33424375a82f5609e0bca
cristian97511 <cristian975112@gmail.com>
2023-03-12 20:40:21 +0100

    Ejercicio reloj echo

--- src/UD6_OOP/EjerciciosII_MasClases/Ejercicio4/Reloj.java
+++ src/UD6_OOP/EjerciciosII_MasClases/Ejercicio4/Reloj.java
@@ -17,9 +17,14 @@ public class Reloj {
     }
 
     public Reloj(int hora, int minuto, int segundo) {
-        this.hora = hora;
-        this.minuto = minuto;
-        this.segundo = segundo;
+        if(comprobarms(minuto, segundo) && comprobar24h(hora)) {
+            this.hora = hora;
+            this.minuto = minuto;
+            this.segundo = segundo;
+        } else {
+            System.err.println("El reloj debe tener" +
+                    "el formato H M S");
+        }
 
     }
 
@@ -39,7 +44,9 @@ public class Reloj {
     }
 
     public void setMinuto(int minuto) {
-        this.minuto = minuto;
+        if(comprobarms(minuto)) {
+            this.minuto = minuto;
+        }
     }
 
     public int getSegundo() {
@@ -59,21 +66,39 @@ public class Reloj {
         this.segundo = segundo;
     }
 
-    private String imprimeHora(int hora, int minuto, int segundo, String txt) {
-        return hora + " : " + minuto + " : " + segundo + txt;
+    private void imprimeHora(int hora, String txt) {
+        System.out.println(hora + " : " + this.minuto + " : " + this.segundo + txt);;
     }
 
-    public void dimeHora24() {
-        System.out.print("Hora en formato de 24h: ");
-        imprimeHora(this.hora, this.minuto, this.segundo, "h");
+    private void dimeHora12() {
+        String txt;
+        int hora;
+        if(this.hora > 12) {
+            txt = "pm";
+            hora = this.hora -12;
+        } else {
+            txt = "am";
+            hora = this.hora;
+        }
+        imprimeHora(hora, txt);
+    }
+
+    private void dimeHora24() {
+        imprimeHora(this.hora, "h");
+    }
+
+    public void dimeHora(int formato) {
+        if(formato == 12) {
+            dimeHora12();
+        } else if(formato == 24) {
+            dimeHora24();
+        }
     }
 
     private boolean enrango(int numero, int maximo, String txt) {
         if(numero <= maximo && numero >= 0) {
             return true;
         } else {
-            System.err.println("Debes introducir un valor menor a" + maximo
-            + " y mayor que 0 en " + txt);
             return false;
         }
     }
@@ -94,13 +119,26 @@ public class Reloj {
         return enrango(hora, 23, "hora");
     }
 
-    private boolean comprobar12h(int hora) {
-        return enrango(hora, 11, "hora");
-    }
 
-    private boolean comprobarh(int hora, int minutos, int segundos, int formato{
-        boolean res = false;
-        if(comprobarms(minutos, segundos))
-    }
 
+    public void tick(int formato) {
+        this.segundo += 1;
+        if(!comprobarms(this.segundo)) {
+            this.segundo = 0;
+            this.minuto += 1;
+        }
+
+        if(!comprobarms(this.minuto)) {
+            this.minuto = 0;
+            this.hora += 1;
+        }
+
+        if(!comprobar24h(this.hora)) {
+            this.hora = 0;
+            this.minuto = 0;
+            this.segundo = 0;
+        }
+
+        dimeHora(formato);
+    }
 }
--- src/UD6_OOP/EjerciciosII_MasClases/Ejercicio4/main.java
+++ src/UD6_OOP/EjerciciosII_MasClases/Ejercicio4/main.java
@@ -3,7 +3,15 @@ package UD6_OOP.EjerciciosII_MasClases.Ejercicio4;
 /**
  * @author cristian
  * @project Ejercicios_Progrmacion - UD6_OOP.EjerciciosII_MasClases.Ejercicio4
- * @created 11/3/23
+ * @created 12/3/23
  */
 public class main {
+    public static void main(String[] args) {
+        Reloj primero = new Reloj(13, 35, 15);
+        primero.dimeHora(24);
+
+        while (true) {
+            primero.tick(12);
+        }
+    }
 }
