ffed317fddc6f0169ec7398f295a945e19bf1dc7
cristian97511 <cristian975112@gmail.com>
2023-03-15 05:14:02 +0100

    creado package AppContactos

---
+++ src/UD6_OOP/AppContactos/Contacto.java
@@ -0,0 +1,48 @@
+package UD6_OOP.AppContactos;
+
+/**
+ * @author cristian
+ * @project Ejercicios_Progrmacion - UD6_OOP.AppContactos
+ * @created 15/3/23
+ */
+public class Contacto {
+    private String nombre, correo;
+    private int Telefono;
+
+    public Contacto(String nombre, String correo, int telefono) {
+        this.nombre = nombre;
+        this.correo = correo;
+        Telefono = telefono;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public int getTelefono() {
+        return Telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        Telefono = telefono;
+    }
+
+    @Override
+    public String toString() {
+        return "Nombre: '" + nombre + "\n" +
+                "Correo: " + correo + "\n" +
+                "Telefono: " + Telefono + "\n";
+    }
+}
