fba64ffa244a5021d4a11f009fb8f7487ffe40d7
cristian97511 <cristian975112@gmail.com>
2023-03-20 23:09:47 +0100

    Queda testing de la app de AppBanco

--- src/UD6_OOP/AppBanco/Banco.java
+++ src/UD6_OOP/AppBanco/Banco.java
@@ -1,78 +1,214 @@
 package UD6_OOP.AppBanco;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
+/**
+ * @author cristian
+ * @project Ejercicios_Progrmacion - UD6_OOP.AppBanco
+ * @created 20/3/23
+ */
 public class Banco {
-    private static int max_cuentas = 100;
-    private static Cuenta[] cuentas = new Cuenta[max_cuentas];
+
+    public static int max_cuentas = 100;
+
     private static int cont = 0;
+    public static Cuenta cuentas[] = new Cuenta[max_cuentas];
 
-    public static String mostrar_cuenta(Cuenta cuenta) {
-        return cuenta.toString();
+    public static void setMax_cuentas(int max_cuentas) {
+        Banco.max_cuentas = max_cuentas;
     }
 
-    public static String mostrar_cuenta(int n) {
-        return n + " " + mostrar_cuenta(cuentas[n]);
-    }
-
-    public static void cabeceraid() {
-        System.out.println("Id" + "\t" + "Titular" + "\t" + "Saldo");
-    }
-
-
-    public static boolean mostrar_banco() {
-        boolean res = false;
-        if(cont == 0) {
-            System.out.println("No hay cuentas que mostrar");
-        } else {
-            for(int i = 0; i<cont; i++) {
-                res = true;
-                mostrar_cuenta(i);
-            }
-        }
-
-        return res;
-    }
-
-
-    public static int pideInt(String txt) {
+    public static int PideInt(String txt) {
         System.out.println(txt);
         return new Scanner(System.in).nextInt();
     }
 
+    public static int mostrar_menu() {
+        System.out.println("1. Ver cuentas");
+        System.out.println("2. Ingresar dinero.");
+        System.out.println("3. Retirar dinero");
+        System.out.println("4. Transferencia");
+        System.out.println("5. Agregar cuenta");
+        System.out.println("6. Eliminar cuenta");
+        System.out.println("7. Buscar cuenta");
+        System.out.println("8. Mostrar morosos");
+        System.out.println("9. Salir");
+        int opcion = PideInt("Selecciona una opcion del 1 al 9");
+        if (cuentas_banco() || opcion == 5) {
+            manejar(opcion);
+        }
+        return opcion;
+    }
 
-    public static Cuenta Elegirid() {
-        if(mostrar_banco()) {
-            System.out.println("Selecciona la id de una cuenta");
-            int id = new Scanner(System.in).nextInt();
-            return SelCuenta(id);
+
+    public static final void Cabeceraid() {
+        System.out.println("Id" + "\t" + "Titular" + "\t" + "Saldo");
+    }
+
+    public static String mostrar_cuenta(int n) {
+        return n + " " + cuentas[n].toString();
+    }
+
+    public static boolean cuentas_banco() {
+        if (cont > 0) {
+            return true;
         } else {
-            mostrar_menu();
-            return null;
-            
+            System.out.println("No hay cuentas en el banco");
+            return false;
         }
-        
     }
 
-    public static Cuenta SelCuenta(int id) {
-        if(id < cont ) {
-            return cuentas[id];
-        } else  {
-            System.out.println("La cuenta que has seleccionado no existe");
-            return null;
+
+    public static boolean buscar() {
+        boolean res = false;
+        System.out.println("Introduce el nombre de la cuenta que buscas:");
+        String Titular = new Scanner(System.in).nextLine();
+        List<String> respuesta = new ArrayList<String>();
+        for (int i = 0; i < cont; i++) {
+            if (cuentas[i].getTitular().equalsIgnoreCase(Titular)) {
+                respuesta.add(mostrar_cuenta(i));
+                res = true;
+            }
+        }
+
+        mostrar_res(respuesta, "No hay cuentas con el nombre de titular indicado");
+        return res;
+    }
+
+
+    public static void mostrar_res(List<String> res, String txt) {
+        if (res.isEmpty()) {
+            System.out.println(txt);
+        } else {
+            for (String cuenta : res) {
+                System.out.println(res);
+            }
         }
     }
-    
+
+    public static int sel_cuenta(String txt) {
+        boolean busqueda;
+        do {
+            busqueda = buscar();
+        } while (!busqueda);
+
+        return PideInt("Selecciona la id de la cuenta " + txt);
+
+    }
+
+
+    public static void transferncia(Cuenta emisora, Cuenta receptora, int cantidad) {
+        if (emisora.retirar(cantidad)) {
+            receptora.ingresar(cantidad);
+            System.out.println("Se ha hecho una transferncia de " + cantidad + "€" +
+                    " desde la cuenta de " + emisora.getTitular() + " a la cuenta de "
+                    + receptora.getTitular());
+        }
+
+        System.out.println(emisora);
+        System.out.println(receptora);
+    }
+
+    public static void Agregar_Cuenta() {
+        System.out.println("Creando una nueva cuenta....");
+        System.out.println("Introduce el nombre del titular de "
+                + " la cuenta");
+        String Titular = new Scanner(System.in).nextLine();
+        int Saldo = PideInt("Introduce el saldo inicial que va a tenr la cuenta");
+        if (Cuenta.comprobar_saldo(Saldo)) {
+            cuentas[cont] = new Cuenta(Titular, Saldo);
+            cont++;
+        }
+    }
+
+
+    public static void eliminar_cuenta(int n) {
+        System.out.println(mostrar_cuenta(n));
+        int seguro = PideInt("Estas seguro de querer eliminar la cuenta con ID " + n + "?" +
+                "(Presiona 0 para confirmar o cualquier otra tecla para cancelar)");
+        if (seguro == 0) {
+            for (int i = n; i < cont - 1; i++) {
+                cuentas[i] = cuentas[i + 1];
+            }
+            System.out.println("Cuenta eliminada correctamente");
+            cont--;
+        } else {
+            System.out.println("Se ha cancelado la eliminacion de cuenta");
+        }
+
+    }
+
+    public static void mostrar_morosos() {
+        List<String> Morosos = new ArrayList<String>();
+        for (int i = 0; i < cont; i++) {
+            if (cuentas[i].getSaldo() < 0) {
+                Morosos.add(cuentas[i].toString());
+            }
+        }
+
+        mostrar_res(Morosos, "No hay morosos");
+    }
+
     public static void manejar(int opcion) {
-        switch(opcion) {
+        int id_cuenta = -1;
+        switch (opcion) {
             case 1:
-                mostrar_banco();
+                Cabeceraid();
+                for (int i = 0; i < cont; i++) {
+                    System.out.println(i + "\t" + cuentas[i]);
+                }
                 break;
             case 2:
-                Elegirid().ingresar();
-                
-                    
+                id_cuenta = sel_cuenta("en la que quieres ingresar");
+                cuentas[id_cuenta].ingresar(PideInt("Indica la cantidad a ingresar"));
+                break;
+            case 3:
+                id_cuenta = sel_cuenta(" de la que quieres retirar");
+                cuentas[id_cuenta].retirar(PideInt("Indica la cantidad que quieres retirar"));
+                break;
+            case 4:
+                id_cuenta = sel_cuenta(" desde la que quieres transferir");
+                Cuenta emisora = cuentas[id_cuenta];
+                id_cuenta = sel_cuenta(" que va a recibir la transferencia");
+                Cuenta receptora = cuentas[id_cuenta];
+                transferncia(emisora, receptora, PideInt("Indica la cantidad a transferir"));
+                break;
+            case 5:
+                Agregar_Cuenta();
+                Cabeceraid();
+                System.out.println((cont -1) + "\t" + cuentas[cont - 1]);
+                break;
+            case 6:
+                id_cuenta = sel_cuenta(" que quieres eliminar");
+                eliminar_cuenta(id_cuenta);
+                break;
+            case 7:
+                buscar();
+                break;
+            case 8:
+                mostrar_morosos();
+                break;
+            case 9:
+                System.out.println("Saliste del menu");
+                break;
+            default:
+                System.out.println("Debes de seleccionar una opcion del "
+                        + " 1 al 9");
         }
+
+        if(opcion == 2 || opcion == 3) {
+            Cabeceraid();
+            System.out.println(id_cuenta + "\t" + cuentas[id_cuenta]);
+        }
+
     }
-    
+
+    public static void main(String[] args) {
+        int opcion;
+        do {
+            opcion = mostrar_menu();
+        } while(opcion != 9);
+    }
 }
--- src/UD6_OOP/AppBanco/Cuenta.java
+++ src/UD6_OOP/AppBanco/Cuenta.java
@@ -1,13 +1,18 @@
 package UD6_OOP.AppBanco;
 
+/**
+ * @author cristian
+ * @project Ejercicios_Progrmacion - UD6_OOP.AppBanco
+ * @created 20/3/23
+ */
 public class Cuenta {
     private String Titular;
     private int Saldo;
 
-    public Cuenta(String titular, Double saldo) {
-        this.Titular = titular;
+    public Cuenta(String titular, int saldo) {
         if(comprobar_saldo(saldo)) {
             this.Saldo = saldo;
+            this.Titular = titular;
         }
     }
 
@@ -29,38 +34,44 @@ public class Cuenta {
         }
     }
 
-    
-    public void ingresar(int saldo)  {
+
+    public String toString() {
+        return this.Titular + "\t" + this.Saldo;
+    }
+
+    private static boolean mayorque(int valor, int min) {
+        if(valor > min) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static boolean comprobar_saldo(int saldo) {
+        if(mayorque(saldo, -100) && mayorque(saldo, 0)) {
+            return true;
+        } else {
+            System.out.println("El saldo debe de ser mayor que 0 y que -100");
+            return false;
+        }
+    }
+
+    public void ingresar(int saldo) {
+        this.setSaldo(this.Saldo + saldo);
+    }
+
+    public boolean retirar(int saldo) {
+        boolean res = false;
         if(comprobar_saldo(saldo)) {
-            this.Saldo += saldo;
+            if(saldo <= this.Saldo) {
+                this.Saldo -= saldo;
+            } else {
+                System.out.println("La cuenta de " + this.Titular
+                + " no tiene fondos suficientes");
+            }
         }
+
+        return res;
     }
 
-    public void retirar(int saldo) {
-        if(mayorque(saldo, this.Saldo))  {
-            this.setSaldo(this.Saldo -= saldo);
-        } else {
-            System.out.println("La cuenta de " + this.Titular + " no tiene"
-            + " fondos suficientes" );
-        }
-    }
-
-
-    private boolean mayorque(int cantidad, int max) {
-        if(cantidad > max) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    private boolean comprobar_saldo(Double cantidad) {
-        if(mayorque(cantidad, -100.0) && mayorque(cantidad, 0.0)) {
-            return true;
-        } else {
-            System.out.println("Debes indicar una cantidad mayor que 0 y que -100");
-            return false;
-        }
-    }
-    
 }
