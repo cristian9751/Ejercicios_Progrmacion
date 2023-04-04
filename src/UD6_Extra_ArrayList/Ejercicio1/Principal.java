f68f2c9eb5368e186fe1a56927f66557732d50b9
cristian9751 <cristian975112@gmail.com>
2023-04-04 14:55:59 +0200

    4 del 4 del 23

---
+++ src/UD6_Extra_ArrayList/Ejercicio1/Principal.java
@@ -0,0 +1,50 @@
+package UD6_Extra_ArrayList.Ejercicio1;
+
+/**
+ * Crea un programa que en su función ‘main’ haga lo siguiente en el orden indicado:
+ * 1. Crear un ArrayList de cadenas de texto (String).
+ * 2. Añadir varios String con diferentes colores (Rojo, Azul, Verde, Naranja y Blanco).
+ * 3. Recorrer/iterar el ArrayList y mostrar por pantalla todos los colores.
+ * 4. Añadir dos colores más al final de la lista (Negro y Cyan).
+ * 5. Añadir dos colores más en la 1ª y 2ª posición (Amarillo y Magenta).
+ * 6. Eliminar los colores de la 3ª y 4ª posición (Rojo y Azul).
+ * 7. Volver a mostrar todos los colores por pantalla.
+ */
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Principal {
+
+    protected static List<String> ListaColores = new ArrayList<>();
+    protected static String[] Colores_Iniciales = {"Rojo", "Azul", "Verde", "Naranja", "Blanco"}; //Array de colores iniciales
+
+    /***
+     * Añade los colres del array de colores iniciales al arraylist ListaColores
+     */
+    protected static void Agregar_Inicial() {
+        for(int  i = 0; i<Colores_Iniciales.length; i++) {
+            ListaColores.add(Colores_Iniciales[i]);
+        }
+    }
+
+    /***
+     * Muestra la lista
+     */
+    protected static void mostrar_lista() {
+        for(String  string : ListaColores) {
+            System.out.println(string);
+        }
+    }
+    protected static void main(String[] args) {
+        Agregar_Inicial();
+        mostrar_lista();
+        ListaColores.add("Negro"); //Añade negro al final de la lista
+        ListaColores.add("Cyan"); //Añade cyan al final de la lista
+        ListaColores.add(1, "Amarillo");
+        ListaColores.add(2, "Magenta");
+        ListaColores.remove("Rojo");
+        ListaColores.remove("Azul");
+        mostrar_lista();
+    }
+}
---
+++ src/UD6_Extra_ArrayList/Ejercicio2/Principal.java
@@ -0,0 +1,110 @@
+package UD6_Extra_ArrayList.Ejercicio2;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Principal {
+    protected static List<Integer> notas = new ArrayList<>();
+    protected static boolean  comprobar_nota(int nota) {
+        boolean res = false;
+        if(nota >= 0 && nota <= 10) {
+            res = true;
+        } else  {
+            System.out.println("La nota debe estar entre 0 y 10. Introduce -1 si quieres dejar de introducir" +
+                    " notas");
+        }
+
+        if(nota == -1) {
+            res = true;
+        }
+
+        return res;
+    }
+
+    protected static void mostrar_notas() {
+        for(Integer nota : notas) {
+            if(nota != -1) {
+                System.out.println(nota);
+            }
+        }
+    }
+
+    protected static int count_aprobados() {
+        int contador = 0;
+        for(Integer nota : notas) {
+            if(nota >= 5 && nota != -1) {
+                contador++;
+            }
+        }
+        return contador;
+    }
+
+    protected static int count_suspendidos() {
+        int contador = 0;
+        for(Integer nota : notas) {
+            if(nota <= 5 && nota != -1) {
+                contador++;
+            }
+        }
+        return contador;
+    }
+
+    protected static  int  PideNota(int num) {
+        int res;
+      do {
+          System.out.println("Introduce la nota " + num);
+          res = new Scanner(System.in).nextInt();
+      } while(!comprobar_nota(res));
+
+      return res;
+    }
+
+
+    protected static int CalcularMedia() {
+        int suma = 0;
+        for(Integer nota : notas) {
+            suma += nota;
+        }
+
+        return suma / (notas.size() -1);
+    }
+
+    protected static void ImprimeExists(int num) {
+        if(existe(num)) {
+            System.out.println("Hay alumnos que tienen " + num);
+        } else {
+            System.out.println("No hay alumnos que tienen " + num);
+        }
+    }
+
+    protected static boolean  existe(int num) {
+        boolean res = false;
+        for(Integer nota : notas) {
+            if(nota == num) {
+                res = true;
+                break;
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int nota;
+        int cont = 1;
+        System.out.println("Puedes introducir notas hasta que introduzcas -1");
+        do {
+            nota = PideNota(cont);
+            notas.add(nota);
+            cont++;
+        } while(nota != -1);
+
+        System.out.println("TODAS LAS NOTAS: ");
+        mostrar_notas();
+        System.out.println("Hay " + count_aprobados() + " alunnos aprobados");
+        System.out.println("hAY " + count_suspendidos() + " alumnos suspendidos");
+        System.out.println("La media de todas las notas es " + CalcularMedia());
+        ImprimeExists(0);
+        ImprimeExists(10);
+    }
+}
